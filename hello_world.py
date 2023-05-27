df79561ee98e1e976da350f0ce6458d15874a619
MacApos <maciejapostol98@gmail.com>
2023-05-28 01:36:31 +0200

    commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print("hello world")
+class LEAD():
+    def __init__(self, one, two):
+        self.first = one
+        self.second = two
+
+apiSeries = LEAD("Jeden", "Dwa")
+formsSeries = LEAD("uno", "Dos")
+
+print(apiSeries.first)
+print(formsSeries.second)
+
+
